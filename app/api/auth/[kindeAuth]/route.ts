0259f80f5dd290824fbab17a25fcaa6e08ccb06c
Anthony DePaul <anthony@adepaul.dev>
2025-03-01 00:49:07 -0500

    Moved kinde auth handler init out of build time

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,3 +1,18 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = handleAuth();
+let kindeAuthHandler: ((req: any, res: any) => any) | null = null;
+
+export const GET = (req: any, res: any) => {
+  if (kindeAuthHandler) { return kindeAuthHandler(req, res); }
+
+  if (process.env.KINDE_CLIENT_ID
+    && process.env.KINDE_CLIENT_SECRET
+    && process.env.KINDE_ISSUER_URL
+    && process.env.KINDE_SITE_URL
+    && process.env.KINDE_POST_LOGOUT_REDIRECT_URL
+    && process.env.KINDE_POST_LOGIN_REDIRECT_URL
+  ) {
+    kindeAuthHandler = handleAuth();
+    return kindeAuthHandler(req, res);
+  }
+};
